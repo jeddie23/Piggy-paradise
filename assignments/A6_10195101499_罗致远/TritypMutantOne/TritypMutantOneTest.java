9604ade023be3a265065023a3a0b9babaca53309
jeddie <1257582342@qq.com>
2021-05-20 02:16:11 +0800

    jeddie's homework

---
+++ assignments/A6_10195101499_罗致远/TritypMutantOne/TritypMutantOneTest.java
@@ -0,0 +1,30 @@
+import org.junit.*;
+
+import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
+
+public class TritypMutantOneTest {
+    TritypMutantOne tri=new TritypMutantOne();
+
+    //    Triang = 1 if triangle is scalene
+    //    Triang = 2 if triangle is isosceles
+    //    Triang = 3 if triangle is equilaterala
+    //    Triang = 4 if not a triangle
+
+    @Test
+    public void testTriang_4(){
+        assertEquals(4,tri.Triang(1,2,7));
+        assertEquals(1,tri.Triang(3,4,5));
+        assertEquals(3,tri.Triang(3,3,3));
+        assertEquals(4,tri.Triang(-1,-1,-1));
+        assertEquals(4,tri.Triang(1,-1,-1));
+        assertEquals(4,tri.Triang(1,1,-1));
+        assertEquals(2,tri.Triang(3,3,5));
+        assertEquals(4,tri.Triang(1,7,2));
+        assertEquals(4,tri.Triang(7,2,1));
+        assertEquals(2,tri.Triang(3,2,2));
+        assertEquals(2,tri.Triang(3,3,4));
+
+    }
+}
