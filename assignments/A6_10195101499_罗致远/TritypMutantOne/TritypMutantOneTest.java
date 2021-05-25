bc56c5c158bb28a7951f8c33bc9b81128adc6523
jeddie <1257582342@qq.com>
2021-05-26 02:34:06 +0800

    jeddie's homework

--- assignments/A6_10195101499_罗致远/TritypMutantOne/TritypMutantOneTest.java
+++ assignments/A6_10195101499_罗致远/TritypMutantOne/TritypMutantOneTest.java
@@ -14,17 +14,9 @@ public class TritypMutantOneTest {
 
     @Test
     public void testTriang_4(){
-        assertEquals(4,tri.Triang(1,2,7));
-        assertEquals(1,tri.Triang(3,4,5));
-        assertEquals(3,tri.Triang(3,3,3));
-        assertEquals(4,tri.Triang(-1,-1,-1));
-        assertEquals(4,tri.Triang(1,-1,-1));
-        assertEquals(4,tri.Triang(1,1,-1));
-        assertEquals(2,tri.Triang(3,3,5));
-        assertEquals(4,tri.Triang(1,7,2));
-        assertEquals(4,tri.Triang(7,2,1));
-        assertEquals(2,tri.Triang(3,2,2));
+        assertEquals(4,tri.Triang(3,3,7));
         assertEquals(2,tri.Triang(3,3,4));
+
 
     }
 }
